1bf7c40ff40b639dae369030db24efdf14930b77
Bhargav Patel <bhargavvalsad2001@gmail.com>
2021-03-26 03:04:59 -0300

    Completed Runtime permission for the camera button. With the proper demonstration required.

--- BhargavAssign33/src/main/java/bhargav/patel/n01373029/Fragment2/PatelFragment.java
+++ BhargavAssign33/src/main/java/bhargav/patel/n01373029/Fragment2/PatelFragment.java
@@ -1,11 +1,17 @@
 //BhargavPatel N01373029 SectionB
 package bhargav.patel.n01373029.Fragment2;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,10 +19,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import bhargav.patel.n01373029.BhargavActivity;
 import bhargav.patel.n01373029.R;
 
 public class PatelFragment extends Fragment {
+
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS= 7;
+    private int STORAGE_PERMISSION_CODE = 1;
 
     Button btnCam;
     @Override
@@ -35,9 +46,60 @@ public class PatelFragment extends Fragment {
         btnCam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(intent);
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+
+                    // To Show a Toast
+                    Toast toast = Toast.makeText(getActivity(), "Permission has been granted", Toast.LENGTH_LONG);
+                    toast.show();
+                    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                    startActivity(intent);
+                }
+                else {
+                    requestStrongPermission();
+                }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                // To Show a Toast
+                Toast toast = Toast.makeText(getActivity(),"Permission has been granted",Toast.LENGTH_LONG);
+                toast.show();
+            }
+            else {
+                // To Show a Toast
+                Toast toast = Toast.makeText(getActivity(),"Permission has been Denied",Toast.LENGTH_LONG);
+                toast.show();
+            }
+
+        }
+    }
+    private void requestStrongPermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+            new AlertDialog.Builder(getActivity())
+                    .setTitle("Required Permissions")
+                    .setMessage("Try again to grant permission ?")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(getActivity(),new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        }
+
+        else {
+            ActivityCompat.requestPermissions(getActivity(),new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
     }
 }
