0fa60ed11f7a14fefeb75c942f3ecaa9124aeec7
Bhargav Patel <bhargavvalsad2001@gmail.com>
2021-03-25 22:36:38 -0300

    Created a button, if clicked would open the camera application.

--- BhargavAssign33/src/main/java/bhargav/patel/n01373029/Fragment2/PatelFragment.java
+++ BhargavAssign33/src/main/java/bhargav/patel/n01373029/Fragment2/PatelFragment.java
@@ -1,22 +1,43 @@
 //BhargavPatel N01373029 SectionB
 package bhargav.patel.n01373029.Fragment2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import bhargav.patel.n01373029.R;
 
 public class PatelFragment extends Fragment {
 
+    Button btnCam;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_patel, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnCam = view.findViewById(R.id.BhargavCamBtn);
+
+        btnCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+            }
+        });
     }
 }
